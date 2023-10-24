e1dc0fd217657c02bfd38d8b89451cecf746fadd
Erick <erickzanardoo@gmail.com>
2023-10-24 07:18:12 -0300

    feat: adding enemies and items back in the game (#16)

--- lib/game/components/components.dart
+++ lib/game/components/components.dart
@@ -1,5 +1,6 @@
 export 'camera_debugger.dart';
-export 'enemies.dart';
-export 'items.dart';
+export 'enemy.dart';
+export 'item.dart';
 export 'player.dart';
 export 'score_label.dart';
+export 'sprite_object_group_builder.dart';
---
+++ lib/game/components/sprite_object_group_builder.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:dash_run/game/dash_run_game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/sprite.dart';
+import 'package:flame_tiled/flame_tiled.dart';
+
+typedef SpriteObjectGroupComponentBuilder = Component Function({
+  required TiledObject tiledObject,
+  required Sprite sprite,
+});
+
+/// A component that given a tileset and object group, creates its components.
+class SpriteObjectGroupBuilder extends Component with HasGameRef<DashRunGame> {
+  SpriteObjectGroupBuilder({
+    required this.tileset,
+    required this.tileLayerName,
+    required this.tilesetPath,
+    required this.componentBuilder,
+  });
+
+  final Tileset tileset;
+  final String tileLayerName;
+  final String tilesetPath;
+  final SpriteObjectGroupComponentBuilder componentBuilder;
+
+  @override
+  FutureOr<void> onLoad() async {
+    await super.onLoad();
+    final firstGid = tileset.firstGid ?? 0;
+    final layer = gameRef.leapMap.getTileLayer<ObjectGroup>(tileLayerName);
+    final tiles = await gameRef.images.load(
+      tilesetPath,
+    );
+
+    for (final object in layer.objects) {
+      final spritesheet = SpriteSheet(
+        image: tiles,
+        srcSize: Vector2.all(gameRef.tileSize),
+      );
+
+      gameRef.leapMap.add(
+        componentBuilder(
+          tiledObject: object,
+          sprite: spritesheet.getSpriteById(
+            (object.gid ?? 0) - firstGid,
+          ),
+        ),
+      );
+    }
+  }
+}
