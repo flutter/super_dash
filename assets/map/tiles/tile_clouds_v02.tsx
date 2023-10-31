e52458e381f613c77b9fbb185222ea2513efbf03
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-10-31 18:53:20 -0400

    Add files via upload
    Tweaked clouds and added slopes

--- assets/map/tiles/tile_clouds_v02.tsx
+++ assets/map/tiles/tile_clouds_v02.tsx
@@ -1,4 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="tile_clouds_v02" tilewidth="64" tileheight="64" tilecount="72" columns="8">
  <image source="tile_clouds_v02.png" width="512" height="576"/>
+ <tile id="26" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="0"/>
+   <property name="RightTop" type="int" value="29"/>
+  </properties>
+ </tile>
+ <tile id="27" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="29"/>
+   <property name="RightTop" type="int" value="64"/>
+  </properties>
+ </tile>
+ <tile id="30" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="64"/>
+   <property name="RightTop" type="int" value="45"/>
+  </properties>
+ </tile>
+ <tile id="31" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="45"/>
+   <property name="RightTop" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="59" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="20"/>
+   <property name="RightTop" type="int" value="64"/>
+  </properties>
+ </tile>
+ <tile id="61" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="64"/>
+   <property name="RightTop" type="int" value="22"/>
+  </properties>
+ </tile>
+ <tile id="67" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="64"/>
+   <property name="RightTop" type="int" value="30"/>
+  </properties>
+ </tile>
 </tileset>
