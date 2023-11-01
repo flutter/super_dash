b40a27fa520fb513de59173ab133ec21f9e11e8a
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-01 14:49:20 -0400

    feat: added Fly property to flying enemies (#51)

---
+++ assets/map/objects/tile_gate_lvl7.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_gate_lvl7" tilewidth="448" tileheight="320" tilecount="1" columns="1">
+ <image source="tile_gate_lvl7.png" width="448" height="320"/>
+</tileset>
