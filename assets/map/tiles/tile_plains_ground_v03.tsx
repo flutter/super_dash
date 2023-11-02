cc5a464c8a2334f092f42568d927358420e9f3ab
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-02 17:24:55 -0400

    feat: map split tweaks

---
+++ assets/map/tiles/tile_plains_ground_v03.tsx
@@ -0,0 +1,74 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_plains_ground_v03" tilewidth="64" tileheight="64" tilecount="168" columns="12">
+ <image source="tile_plains_ground_v03.png" width="768" height="896"/>
+ <tile id="12" type="Platform"/>
+ <tile id="13" type="Platform"/>
+ <tile id="14" type="Platform"/>
+ <tile id="15" type="Platform"/>
+ <tile id="16" type="Platform"/>
+ <tile id="17" type="Platform"/>
+ <tile id="19" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="0"/>
+   <property name="RightTop" type="int" value="52"/>
+  </properties>
+ </tile>
+ <tile id="20" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="52"/>
+   <property name="RightTop" type="int" value="64"/>
+  </properties>
+ </tile>
+ <tile id="21" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="64"/>
+   <property name="RightTop" type="int" value="0"/>
+  </properties>
+ </tile>
+ <tile id="36" type="Platform"/>
+ <tile id="37" type="Platform"/>
+ <tile id="38" type="Platform"/>
+ <tile id="39" type="Platform"/>
+ <tile id="40" type="Platform"/>
+ <tile id="41" type="Platform"/>
+ <tile id="43" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="0"/>
+   <property name="RightTop" type="int" value="45"/>
+  </properties>
+ </tile>
+ <tile id="44" type="Slope">
+  <properties>
+   <property name="LeftTop" type="int" value="45"/>
+   <property name="RightTop" type="int" value="64"/>
+  </properties>
+ </tile>
+ <tile id="45" type="Platform"/>
+ <tile id="46" type="Platform"/>
+ <tile id="80" type="Platform"/>
+ <tile id="81" type="Platform"/>
+ <tile id="84" type="Platform"/>
+ <tile id="85" type="Platform"/>
+ <tile id="87" type="Platform"/>
+ <tile id="89" type="Platform"/>
+ <tile id="90" type="Platform"/>
+ <tile id="105" type="Hazard"/>
+ <tile id="106" type="Hazard"/>
+ <tile id="108" type="Platform"/>
+ <tile id="109" type="Platform"/>
+ <tile id="111" type="Platform"/>
+ <tile id="112" type="Platform"/>
+ <tile id="113" type="Platform"/>
+ <tile id="127" type="Platform"/>
+ <tile id="128" type="Platform"/>
+ <tile id="129" type="Platform"/>
+ <tile id="130" type="Platform"/>
+ <tile id="131" type="Platform"/>
+ <tile id="145" type="Hazard"/>
+ <tile id="153" type="Platform"/>
+ <tile id="154" type="Platform"/>
+ <tile id="155" type="Hazard"/>
+ <tile id="163" type="Hazard"/>
+ <tile id="164" type="Hazard"/>
+ <tile id="167" type="Hazard"/>
+</tileset>
