a76374cafaaeed06e7b44eb78a3865f3ce0ccf09
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-11-06 17:56:53 +0100

    feat: update game over view

--- lib/game_over/view/game_over_view.dart
+++ lib/game_over/view/game_over_view.dart
@@ -4,6 +4,7 @@ import 'package:dash_run/game_intro/game_intro.dart';
 import 'package:dash_run/gen/assets.gen.dart';
 import 'package:dash_run/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class GameOverPage extends StatelessWidget {
   const GameOverPage({required this.score, super.key});
@@ -19,6 +20,7 @@ class GameOverPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final l10n = context.l10n;
+    const titleColor = Color(0xFF18274C);
 
     return PageWithBackground(
       background: const GameBackground(),
@@ -32,16 +34,47 @@ class GameOverPage extends StatelessWidget {
         child: Column(
           children: [
             const Spacer(flex: 15),
-            Text(l10n.gameOver),
-            Text(l10n.betterLuckNextTime),
+            Text(
+              l10n.gameOver,
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: titleColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
+            Text(
+              l10n.betterLuckNextTime,
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: titleColor,
+                  ),
+            ),
             const Spacer(flex: 4),
-            Text(l10n.totalScore),
+            Text(
+              l10n.totalScore,
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: titleColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+            ),
             const Spacer(flex: 2),
-            GameElevatedButton(label: '$score Pts'),
+            _ScoreWidget(score),
             const Spacer(flex: 4),
-            GameElevatedButton(label: l10n.submitScore),
+            GameElevatedButton(
+              label: l10n.submitScore,
+              onPressed: () {},
+            ),
             const Spacer(flex: 3),
-            GameElevatedButton(label: l10n.playAgain),
+            GameElevatedButton(
+              label: l10n.playAgain,
+              onPressed: () {},
+              gradient: const LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color(0xFFA6C3DF),
+                  Color(0xFF79AACA),
+                ],
+              ),
+            ),
             const Spacer(flex: 40),
             const BottomBar(),
             const SizedBox(height: 16),
@@ -49,5 +82,56 @@ class GameOverPage extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class _ScoreWidget extends StatelessWidget {
+  const _ScoreWidget(this.score);
+
+  final int score;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0x80EAFFFE),
+            Color(0x80C9D9F1),
+          ],
+        ),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Icon(Icons.star, color: Colors.amber),
+          const SizedBox(width: 8),
+          RichText(
+            text: TextSpan(
+              style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: const Color(0xFF4D5B92),
+                    fontWeight: FontWeight.bold,
+                  ),
+              children: [
+                TextSpan(text: '${formatScore(score)} '),
+                const TextSpan(
+                  text: 'Pts',
+                  style: TextStyle(fontSize: 24),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  String formatScore(int score) {
+    final formatter = NumberFormat('#,###');
+    return formatter.format(score);
   }
 }
