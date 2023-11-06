cf3b8fe892e2804461fedeb79dc1fdf08fbd97e3
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-06 18:15:08 -0500

    enemy spritesheets

---
+++ assets/map/anim/spritesheet_enemy_butterfly.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet_enemy_butterfly" tilewidth="64" tileheight="64" tilecount="14" columns="7">
+ <image source="spritesheet_enemy_butterfly.png" width="448" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="42"/>
+   <frame tileid="1" duration="42"/>
+   <frame tileid="2" duration="42"/>
+   <frame tileid="3" duration="42"/>
+   <frame tileid="4" duration="42"/>
+   <frame tileid="5" duration="42"/>
+   <frame tileid="6" duration="42"/>
+   <frame tileid="7" duration="42"/>
+   <frame tileid="8" duration="42"/>
+   <frame tileid="9" duration="42"/>
+   <frame tileid="10" duration="42"/>
+   <frame tileid="11" duration="42"/>
+   <frame tileid="12" duration="42"/>
+   <frame tileid="13" duration="42"/>
+  </animation>
+ </tile>
+</tileset>
