5efe82f978f0c3f5fd608e7e377330b8e79864bc
Marcos Sevilla <marcossevilla14@gmail.com>
2023-11-06 23:18:00 +0100

    chore: apply feedback

--- lib/game/bloc/game_event.dart
+++ lib/game/bloc/game_event.dart
@@ -1,25 +1,39 @@
 part of 'game_bloc.dart';
 
-sealed class GameEvent {}
+sealed class GameEvent extends Equatable {
+  const GameEvent();
+
+  @override
+  List<Object?> get props => [];
+}
 
 final class GameScoreIncreased extends GameEvent {
-  GameScoreIncreased({this.by = 1});
+  const GameScoreIncreased({this.by = 1});
 
   final int by;
+
+  @override
+  List<Object> get props => [by];
 }
 
 final class GameScoreDecreased extends GameEvent {
-  GameScoreDecreased({this.by = 1});
+  const GameScoreDecreased({this.by = 1});
 
   final int by;
+
+  @override
+  List<Object> get props => [by];
 }
 
 final class GameOver extends GameEvent {
-  GameOver();
+  const GameOver();
 }
 
 final class GameSectionCompleted extends GameEvent {
-  GameSectionCompleted({required this.sectionCount});
+  const GameSectionCompleted({required this.sectionCount});
 
   final int sectionCount;
+
+  @override
+  List<Object> get props => [sectionCount];
 }
