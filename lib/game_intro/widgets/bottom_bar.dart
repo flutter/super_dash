4514682d2dc3b7791ad53bbd2ec561904300a357
Jaime Sanchez <jaimesanchez8@gmail.com>
2023-11-06 12:39:34 +0100

    feat: extract bottom bar

---
+++ lib/game_intro/widgets/bottom_bar.dart
@@ -0,0 +1,51 @@
+import 'package:dash_run/constants/constants.dart';
+import 'package:dash_run/game_intro/game_intro.dart';
+import 'package:dash_run/l10n/l10n.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class BottomBar extends StatelessWidget {
+  const BottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final l10n = context.l10n;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 32),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const AudioButton(),
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                l10n.superDash,
+                style: theme.textTheme.titleMedium?.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 4),
+              RichText(
+                text: TextSpan(
+                  text: l10n.howItsMade,
+                  style: theme.textTheme.titleMedium?.copyWith(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () => launchUrlString(Urls.howWeBuilt),
+                ),
+              ),
+            ],
+          ),
+          const InfoButton(),
+        ],
+      ),
+    );
+  }
+}
