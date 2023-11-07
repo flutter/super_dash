2b81af61a8c603374fa5976297520d42d41b386d
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-07 18:04:35 -0500

    animation updates

---
+++ assets/map/anim/spritesheet_enemy_ant.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="spritesheet_enemy_ant" tilewidth="64" tileheight="64" tilecount="12" columns="6">
+ <image source="spritesheet_enemy_ant.png" width="384" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="42"/>
+   <frame tileid="1" duration="42"/>
+   <frame tileid="2" duration="42"/>
+   <frame tileid="3" duration="42"/>
+   <frame tileid="4" duration="42"/>
+   <frame tileid="5" duration="42"/>
+   <frame tileid="6" duration="42"/>
+   <frame tileid="7" duration="42"/>
+   <frame tileid="8" duration="42"/>
+   <frame tileid="9" duration="42"/>
+   <frame tileid="10" duration="42"/>
+   <frame tileid="11" duration="42"/>
+  </animation>
+ </tile>
+</tileset>
