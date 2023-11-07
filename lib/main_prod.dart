1b62acd00df1bee18ae8c7fe88afb32065868d04
Erick <erickzanardoo@gmail.com>
2023-11-07 10:42:10 -0300

    feat: prod firebase config

---
+++ lib/main_prod.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dash_run/app/app.dart';
+import 'package:dash_run/audio/audio.dart';
+import 'package:dash_run/bootstrap.dart';
+import 'package:dash_run/firebase_options_prod.dart';
+import 'package:dash_run/settings/persistence/persistence.dart';
+import 'package:dash_run/settings/settings.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:leaderboard_repository/leaderboard_repository.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final settings = SettingsController(
+    persistence: LocalStorageSettingsPersistence(),
+  );
+
+  final audio = AudioController()..attachSettings(settings);
+
+  await audio.initialize();
+
+  final leaderboardRepository = LeaderboardRepository(
+    FirebaseFirestore.instance,
+  );
+
+  unawaited(
+    bootstrap(
+      () => App(
+        audioController: audio,
+        settingsController: settings,
+        leaderboardRepository: leaderboardRepository,
+      ),
+    ),
+  );
+}
