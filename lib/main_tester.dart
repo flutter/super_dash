8b721724fc1011a3934aa725d62947d1324766b1
Erick <erickzanardoo@gmail.com>
2023-11-07 10:58:30 -0300

    removing older files

--- lib/main_tester.dart
+++ lib/main_tester.dart
@@ -4,7 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dash_run/app/app.dart';
 import 'package:dash_run/audio/audio.dart';
 import 'package:dash_run/bootstrap.dart';
-import 'package:dash_run/firebase_options.dart';
+import 'package:dash_run/firebase_options_dev.dart';
 import 'package:dash_run/settings/persistence/persistence.dart';
 import 'package:dash_run/settings/settings.dart';
 import 'package:firebase_core/firebase_core.dart';
