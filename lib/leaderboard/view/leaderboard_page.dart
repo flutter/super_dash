8f341798cdec8ff5cc5ab1267e720e1624aa1696
Marcos Sevilla <31174242+marcossevilla@users.noreply.github.com>
2023-11-08 12:45:06 +0100

    feat: add leaderboard UI (#92)

---
+++ lib/leaderboard/view/leaderboard_page.dart
@@ -0,0 +1,133 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:dash_run/game/game.dart';
+import 'package:dash_run/gen/assets.gen.dart';
+import 'package:dash_run/l10n/l10n.dart';
+import 'package:dash_run/score/score.dart';
+import 'package:flow_builder/flow_builder.dart';
+import 'package:flutter/material.dart';
+
+enum LeaderboardStep { gameIntro, gameScore }
+
+class LeaderboardPage extends StatelessWidget {
+  const LeaderboardPage({
+    this.step = LeaderboardStep.gameIntro,
+    super.key,
+  });
+
+  static Page<void> page() {
+    return const MaterialPage(
+      child: LeaderboardPage(),
+    );
+  }
+
+  static PageRoute<void> route() {
+    return PageRouteBuilder(
+      pageBuilder: (_, __, ___) => const LeaderboardPage(),
+    );
+  }
+
+  final LeaderboardStep step;
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return PageWithBackground(
+      background: const GameBackground(),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: Assets.images.leaderboardBg.image(
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: MediaQuery.sizeOf(context).height * .3,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Leaderboard(),
+                const SizedBox(height: 20),
+                if (step == LeaderboardStep.gameScore)
+                  GameElevatedButton.icon(
+                    label: l10n.playAgain,
+                    icon: const Icon(Icons.replay),
+                    onPressed: () => context.flow<ScoreState>().complete,
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Leaderboard extends StatelessWidget {
+  const Leaderboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.sizeOf(context);
+    return Container(
+      height: size.height * .5,
+      margin: const EdgeInsets.symmetric(horizontal: 60),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color(0xFF4E4F65),
+            Color(0xFF1B1B36),
+          ],
+        ),
+      ),
+      child: _LeaderboardContent(),
+    );
+  }
+}
+
+class _LeaderboardContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    final theme = Theme.of(context);
+    final listTileTheme = theme.listTileTheme;
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            l10n.leaderboardPageLeaderboardHeadline,
+            style: theme.textTheme.headlineSmall?.copyWith(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Flexible(
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: Text('#$index'),
+                  title: Text('$index'),
+                  trailing: Text(l10n.gameScoreLabel(index * 1000)),
+                  titleTextStyle: listTileTheme.titleTextStyle?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  leadingAndTrailingTextStyle:
+                      listTileTheme.leadingAndTrailingTextStyle?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/leaderboard/view/view.dart
@@ -0,0 +1,1 @@
+export 'leaderboard_page.dart';
