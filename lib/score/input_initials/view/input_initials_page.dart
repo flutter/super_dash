eae059b75480e3468c3e2c3998717dc03fabf169
Jaime <52668514+jsgalarraga@users.noreply.github.com>
2023-11-08 17:37:20 +0100

    feat: input initials (#104)

--- lib/score/input_initials/view/input_initials_page.dart
+++ lib/score/input_initials/view/input_initials_page.dart
@@ -1,4 +1,11 @@
+import 'package:app_ui/app_ui.dart';
+import 'package:dash_run/game/game.dart';
+import 'package:dash_run/game_intro/game_intro.dart';
+import 'package:dash_run/gen/assets.gen.dart';
+import 'package:dash_run/score/score.dart';
+import 'package:dash_run/utils/utils.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InputInitialsPage extends StatelessWidget {
   const InputInitialsPage({super.key});
@@ -11,6 +18,54 @@ class InputInitialsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PageWithBackground(
+      background: const GameBackground(),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: Assets.images.gameOverBg.provider(),
+            fit: BoxFit.cover,
+            alignment:
+                isDesktop ? const Alignment(0, .7) : Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              const Spacer(flex: 3),
+              SizedBox(
+                height: 52,
+                width: 52,
+                child: TraslucentBackground(
+                  gradient: const [
+                    Color(0xFFB1B1B1),
+                    Color(0xFF363567),
+                  ],
+                  border: Border.all(color: Colors.white),
+                  child: const Align(
+                    alignment: Alignment(-0.06, 0),
+                    child: Icon(
+                      FontAwesomeIcons.trophy,
+                      size: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Enter your initials for\nthe leaderboard',
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+              const InitialsFormView(),
+              const Spacer(),
+              const BottomBar(),
+              const SizedBox(height: 16),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
