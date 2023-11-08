9b8d0e03c42d73a472471d65a080f4401bf506f4
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-08 15:12:09 -0500

    bush, hill, mountain update

---
+++ assets/map/tiles/tile_plains_hills_v02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_plains_hills_v02" tilewidth="64" tileheight="64" tilecount="108" columns="27">
+ <image source="tile_plains_hills_v02.png" width="1728" height="256"/>
+</tileset>
