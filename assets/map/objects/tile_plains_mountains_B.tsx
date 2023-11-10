3f3d3ee9a3ffc8e0d9cc9843d04c1f67c63af63f
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-10 17:51:20 -0500

    new big mountain

---
+++ assets/map/objects/tile_plains_mountains_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_plains_mountains_B" tilewidth="832" tileheight="576" tilecount="1" columns="1">
+ <image source="tile_plains_mountains_B.png" width="832" height="576"/>
+</tileset>
