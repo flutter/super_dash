26249358f36fbae6618e470eea0f71ab93981907
amyxuhopr <148802436+amyxuhopr@users.noreply.github.com>
2023-11-10 17:50:14 -0500

    3D updates reupload

---
+++ assets/map/tiles/tile_forest_bushes_v03.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_forest_bushes_v03" tilewidth="64" tileheight="64" tilecount="115" columns="23">
+ <image source="tile_forest_bushes_v03.png" width="1472" height="320"/>
+</tileset>
