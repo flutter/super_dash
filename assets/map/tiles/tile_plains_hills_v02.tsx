a576f7aa2b4deb59f1c0ba9e25c8ab02bf66a4a6
Erick Zanardo <erickzanardoo@gmail.com>
2023-11-23 16:15:03 -0300

    second section done

--- assets/map/tiles/tile_plains_hills_v02.tsx
+++ assets/map/tiles/tile_plains_hills_v02.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="tile_plains_hills_v02" tilewidth="64" tileheight="64" tilecount="216" columns="27">
- <image source="tile_plains_hills_v02.png" width="1728" height="256"/>
+<tileset version="1.10" tiledversion="1.10.2" name="tile_plains_hills_v02" tilewidth="64" tileheight="64" spacing="2" tilecount="216" columns="27">
+ <image source="tile_plains_hills_v02.png" width="1782" height="528"/>
 </tileset>
