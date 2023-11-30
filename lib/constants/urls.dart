9233bd66349988440c5fda58bad163142f3f726a
Erick <erickzanardoo@gmail.com>
2023-11-30 10:30:37 -0300

    feat: apps url links (#176)

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -7,5 +7,12 @@ class Urls {
 
   static const String termsOfService = 'https://policies.google.com/terms';
 
-  static const String howWeBuilt = 'https://medium.com/flutter/9c7aa2a5ad31';
+  static const String howWeBuilt =
+      'https://medium.com/flutter/how-we-built-the-new-super-dash-demo-in-flutter-and-flame-in-just-six-weeks-9c7aa2a5ad31';
+
+  static const appStoreLink =
+      'https://apps.apple.com/app/super-dash-flutter-game/id6473050393';
+
+  static const playStoreLink =
+      'https://play.google.com/store/apps/details?id=com.verygoodventures.super_dash';
 }
